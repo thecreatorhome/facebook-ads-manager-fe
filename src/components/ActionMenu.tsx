8a48100985994993679ec031ae5009a97f4f9db9
vivek <vivek>
2025-05-21 14:25:32 +0530

    Menu Action added

--- src/components/ActionMenu.tsx
+++ src/components/ActionMenu.tsx
@@ -1,8 +1,72 @@
 import "../styles/components/ActionMenu.css";
+import { DownOutlined, SettingOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Dropdown, Space } from 'antd';
+
+const items: MenuProps['items'] = [
+    {
+        key: 'duplicate',
+        label: 'Duplicate',
+        extra: 'Ctrl+Shift+D',
+    },
+    {
+        key: 'quickDuplicate',
+        label: 'Quick Duplicate',
+        extra: 'Ctrl+D',
+    },
+    {
+        key: 'copy',
+        label: 'Copy',
+        extra: 'Ctrl+C',
+    },
+    {
+        key: 'paste',
+        label: 'Paste',
+        disabled: true,
+        extra: 'Ctrl+V',
+    },
+    {
+        key: 'delete',
+        label: 'Delete',
+        extra: 'Ctrl+Del',
+    },
+    {
+        type: 'divider',
+    },
+    {
+        key: 'createAdSet',
+        label: 'Create ad set',
+    },
+    {
+        key: 'createRule',
+        label: 'Create rule',
+    },
+    {
+        type: 'divider',
+    },
+    {
+        key: 'id',
+        label: 'ID: 32876387147987439',
+        extra: 'copy',
+        disabled: true
+    },
+];
 
 const ActionMenu = () => {
     return (
-       <div>ActionMenu</div>
+        <Dropdown menu={{ items }}>
+            <a onClick={(e) => e.preventDefault()}>
+                <Space>
+                    <div className="actionOptionContainer">
+                        <div className="actionDots">
+                            <i className="dotActionMenu"></i>
+                            <i className="dotActionMenu"></i>
+                            <i className="dotActionMenu"></i>
+                        </div>
+                    </div>
+                </Space>
+            </a>
+        </Dropdown>
     );
 };
 
--- src/screens/Campaigns.tsx
+++ src/screens/Campaigns.tsx
@@ -4,11 +4,13 @@ import FacebookStories from "../components/FacebookStories";
 import AdsOnFacebookReel from "../components/AdsOnFacebookReel";
 import FacebookInStreamVideo from "../components/FacebookInStreamVideo";
 import BreadCrumb from "../components/BreadCrumb";
+import ActionMenu from "../components/ActionMenu";
 
 const Campaigns = () => {
     return (
         <>
             <BreadCrumb />
+            <ActionMenu />
             <InstagramStories 
             imageUrl="https://www.facebook.com/ads/image/?d=AQIgwWaK-ouHhRj5sSn6gSdPPXUrC7qggdKVxm_NOul_J3GP3H4zLMYdIEzDKlDcnVye5ekU-tjIAoBmbs_JxLzEQkfP_KtUOfLM4VKiTjhXaUnkRtvs7AUwrU7XiSfbyC3AhcMHdf6pVtNAXaWgTsFo" 
             description="Repairing your roof can help you save a lot of money in the long ru...More"/>
